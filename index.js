40e75e8b2ff5ea90cd7f01c53667c2168102b754
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-08-23 14:48:54 +0200

    fix standard

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var util = require('util')
 var c = require('chalk')
 var rc = require('./lib/rc')
 var stack = require('./lib/stack')(rc)
 
-var create_id = validate(rc.create || rc.c)
-if (create_id) return stack.create(create_id, function (err) {
-  if (!err) console.log(c.green('created', create_id))
-  end(err)
-})
-
-var apply_ids = validate(rc.apply || rc.a)
-if (apply_ids) {
-  console.log(c.yellow('applying', apply_ids))
-  return stack.apply(apply_ids, end)
-}
-
-var rm_id = validate(rc.rm)
-if (rm_id) return stack.remove(rm_id, function (err) {
-  if (!err) console.log(c.green('removed', rm_id))
-  end(err)
-})
-
+var createId = validate(rc.create || rc.c)
+var applyIds = validate(rc.apply || rc.a)
+var rmId = validate(rc.rm)
 var list = rc.list || rc.l
-if (list) return stack.list(end)
 
-function validate(str) {
-  if (typeof str == 'string' && str.length > 0) return str
+if (createId) {
+  stack.create(createId, function (err) {
+    if (!err) console.log(c.green('created', createId))
+    end(err)
+  })
+} else if (applyIds) {
+  console.log(c.yellow('applying', applyIds))
+  stack.apply(applyIds, end)
+} else if (rmId) {
+  stack.remove(rmId, function (err) {
+    if (!err) console.log(c.green('removed', rmId))
+    end(err)
+  })
+} else if (list) {
+  stack.list(end)
+} else {
+  console.log('usage: mkstack [' + c.yellow('options') + ']')
+  console.log(c.yellow('  -c, --create  :id') + '  create a new stack')
+  console.log(c.yellow('  -a, --apply   :id') + '  apply a stack')
+  console.log(c.yellow('      --rm      :id') + '  remove a stack')
+  console.log(c.yellow('  -l, --list') + '         list saved stacks')
 }
 
-function end(err, info) {
+function validate (str) {
+  if (typeof str === 'string' && str.length > 0) return str
+}
+
+function end (err, info) {
   if (err) {
     console.error(c.red(err.message || err))
   }
@@ -40,9 +45,3 @@ function end(err, info) {
   }
   process.exit(err ? err.code || 1 : 0)
 }
-
-console.log('usage: mkstack [' + c.yellow('options') + ']')
-console.log(c.yellow('  -c, --create  :id') + '  create a new stack')
-console.log(c.yellow('  -a, --apply   :id') + '  apply a stack')
-console.log(c.yellow('      --rm      :id') + '  remove a stack')
-console.log(c.yellow('  -l, --list') + '         list saved stacks')
